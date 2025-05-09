8a776ed0fae6256d4ec9d38625100943e2f8a357
류형철 <fbgudcjf2@gmail.com>
2025-05-09 15:46:32 +0900

    필수: Level.2

--- src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
+++ src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
@@ -1,8 +1,10 @@
 package org.example.expert.domain.user.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserResponse {
 
     private final Long id;
